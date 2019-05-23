a463cf76948660b01c3de49814591386c84fa185
Anirudh Personal <anirudh.modi2@gmail.com>
2019-05-24 00:06:30 +0530

    Changes

--- server/migrations/20190522133310-products.js
+++ server/migrations/20190522133310-products.js
@@ -13,54 +13,66 @@ module.exports = {
 
 		return sequelize.transaction(t => {
 			var options = {
-				transaction:t
+				transaction: t
 			};
 
 			return queryInterface
-				.createTable("products", 
-				{
-					id: {
-						type: Sequelize.INTEGER,
-						primaryKey: true,
-						autoIncrement: true
-					},
-					// attributes
-					title: {
-						type: Sequelize.TEXT,
-						allowNull: false
-					},
-					description:{
-						type: Sequelize.TEXT
-					},
-					company:{
-						type: Sequelize.TEXT,
-						allowNull: false
-					},
-					created_at: {
-						type: Sequelize.DATE,
-						allowNull:false,
-						defaultValue: Sequelize.NOW
-					},
-					updated_at: {
-						type: Sequelize.DATE,
-						allowNull:false,
-						defaultValue: Sequelize.NOW
-					}
-				},options)
-				.then(()=>{
+				.createTable("products",
+					{
+						id: {
+							type: Sequelize.INTEGER,
+							primaryKey: true,
+							autoIncrement: true
+						},
+						// attributes
+						title: {
+							type: Sequelize.TEXT,
+							allowNull: false
+						},
+						description: {
+							type: Sequelize.TEXT
+						},
+						company: {
+							type: Sequelize.TEXT,
+							allowNull: false
+						},
+						created_at: {
+							type: Sequelize.DATE,
+							allowNull: false,
+							defaultValue: Sequelize.NOW
+						},
+						updated_at: {
+							type: Sequelize.DATE,
+							allowNull: false,
+							defaultValue: Sequelize.NOW
+						}
+					}, options)
+				.then(() => {
 					return queryInterface
 						.bulkInsert('products', [{
 							title: 'Black sneakers',
 							description: 'A black rugged puma shoes',
-							company:'PUMA',
-							created_at:'NOW()',
-							updated_at:'NOW()'
-						},{
+							company: 'PUMA',
+							created_at: 'NOW()',
+							updated_at: 'NOW()'
+						}, {
 							title: 'Blue sneakers',
 							description: 'A blue walking addidas shoes',
-							company:'ADDIDAS',
-							created_at:'NOW()',
-							updated_at:'NOW()'
+							company: 'ADDIDAS',
+							created_at: 'NOW()',
+							updated_at: 'NOW()'
+						}, {
+							title: 'Colgate Charcoal',
+							description: 'A charcoal based toothpaste',
+							company: 'Colgate',
+							created_at: 'NOW()',
+							updated_at: 'NOW()'
+						}, {
+							title: 'One plus 7 pro',
+							description: 'A blazing fast flagship killer',
+							company: 'OnePlus',
+							created_at: 'NOW()',
+							updated_at: 'NOW()'
 						}], options);
 				})
 		});
